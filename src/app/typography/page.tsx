15498baa8348985569efa9c513c3bfe0c7719c74
Aleks Gomeniuk <aleksei.gomeniuk@gmail.com>
2024-09-20 15:10:49 +0100

    refactor: typography page separate constants

--- src/app/typography/page.tsx
+++ src/app/typography/page.tsx
@@ -1,83 +1,15 @@
 import { CustomLink, Heading, Like, MinuteTimer, MonthTimer, Paragraph } from '@/components'
 import { SingleCard } from '@/ui'
 
-import pic from '../../ui/SingleCard/pic.jpg'
-
-const minuteTimerExample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-const monthTimerExample = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-const headingsExample = [
-	{ tag: 'h1' as const, text: 'large title' },
-	{ tag: 'h2' as const, text: 'medium title' },
-	{ tag: 'h3' as const, text: 'small title' },
-]
-
-const paragraphsExample = [
-	{
-		appearance: 's' as const,
-		text: 'SMALL => Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, dignissimos.',
-	},
-	{
-		appearance: 'm' as const,
-		text: 'MEDIUM => Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, dignissimos.',
-	},
-	{
-		appearance: 'l' as const,
-		text: 'LARGE => Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt, dignissimos.',
-	},
-]
-
-const linksExample = [
-	{ type: 'arrow' as const, href: '#', text: 'Читать' },
-	{ type: 'basic' as const, href: '#', text: 'Читать' },
-]
-
-const LikesExample = [
-	{ appearance: 'circle' as const, value: 'Понравилось? Жми' },
-	{ appearance: 'none' as const, value: 5 },
-]
-
-const singleCardsExample = [
-	{
-		course: 'Front-end',
-		duration: 1,
-		heading: 'Как работать с CSS Grid',
-		img: {
-			alt: 'frontEnd course',
-			src: pic,
-		},
-		link: { src: '#', text: 'Читать' },
-		paragraph:
-			'Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы..',
-		rating: 4,
-		time: 3,
-	},
-	{
-		course: 'Back-end',
-		duration: 5,
-		heading: 'Как работать с NextJS',
-		img: {
-			alt: 'Back-end course',
-			src: pic,
-		},
-		link: { src: '#', text: 'Читать' },
-		paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, consectetur.',
-		rating: 42,
-		time: 11,
-	},
-	{
-		course: 'design',
-		duration: 1,
-		heading: 'Как работать с Figma',
-		img: {
-			alt: 'design course',
-			src: pic,
-		},
-		link: { src: '#', text: 'Читать' },
-		paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit at autem est, magnam doloribus neque!',
-		rating: 22,
-		time: 6,
-	},
-]
+import {
+	headingsExample,
+	LikesExample,
+	linksExample,
+	minuteTimerExample,
+	monthTimerExample,
+	paragraphsExample,
+	singleCardsExample,
+} from './examples'
 
 function TypographyPage() {
 	return (
